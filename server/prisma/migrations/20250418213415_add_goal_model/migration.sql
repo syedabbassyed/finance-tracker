1cf6a2cdeb91b7b2e10b884e2a271b7bbee0779b
Syed Abbas <syedabbassyed786@gmail.com>
2025-04-19 03:04:59 +0530

    Goal Model added in Schema

---
+++ server/prisma/migrations/20250418213415_add_goal_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Goal" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "targetAmount" DOUBLE PRECISION NOT NULL,
+    "currentAmount" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "targetDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Goal_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Goal" ADD CONSTRAINT "Goal_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
