d75e1b2200595053f052783f5e8456d7355732a7
Syed Abbas <syedabbassyed786@gmail.com>
2025-04-23 10:02:46 +0530

    Modular Schema setup done

--- server/src/schema/typeDefs.ts
+++ server/src/schema/typeDefs.ts
@@ -1,54 +1,11 @@
-export const typeDefs = `#graphql
-  type User {
-    id: String!
-    name: String!
-    email: String!
-  }
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-  type AuthPayload {
-    token: String!
-    user: User!
-  }
+// get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  type Goal {
-    id: String!
-    title: String!
-    targetAmount: Float!
-    currentAmount: Float!
-    targetDate: String!
-    createdAt: String!
-  }
-
-  type Account {
-    id: String!
-    name: String!
-    balance: Float!
-    createdAt: String!
-    type: AccountType!
-  }
-
-  type AccountType {
-    id: String!
-    name: String!
-  }
-
-  type Query {
-    hello: String!
-    me: User
-    getGoals: [Goal!]!
-    getAccounts: [Account!]!
-    getAccountTypes: [AccountType!]!
-  }
-
-  type Mutation {
-    signup(name: String!, email: String!, password: String!): AuthPayload!
-    login(email: String!, password: String!): AuthPayload!
-    createGoal(title: String!, targetAmount: Float!, targetDate: String!): Goal!
-    updateGoalProgress(goalId: String!, amount: Float!): Goal!
-    editGoal(goalId: String!, title: String, targetAmount: Float, targetDate: String): Goal!
-    deleteGoal(goalId: String!): Goal!
-    createAccount(name: String!, typeId: String!): Account!
-    deleteAccount(accountId: String!): Account!
-    editAccount(accountId: String!, name: String, typeId: String): Account!
-  }
-`;
+const typesArray = loadFilesSync(path.join(__dirname, './**/*.graphql'));
+export const typeDefs = mergeTypeDefs(typesArray);
